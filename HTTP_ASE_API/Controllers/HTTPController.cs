e4342aba325b27d69e7aff99bf8c45218e0519e4
Jeff Hollan <jeffhollan@outlook.com>
2015-10-22 14:21:08 -0700

    initial

---
+++ HTTP_ASE_API/Controllers/HTTPController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TRex.Metadata;
+
+namespace HTTP_ASE_API.Controllers
+{
+    public class HTTPController : ApiController
+    {
+        [Metadata("GET", "HTTP GET")]
+        [HttpGet, Route("api/http")]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> Get(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.GetAsync(url);
+            }
+        }
+
+        [Metadata("POST", "HTTP POST")]
+        [HttpPost, Route("api/http")]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> Post(string url, string body, string contenttype)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.PostAsync(url, new StringContent(body, System.Text.Encoding.UTF8, contenttype));
+            }
+        }
+
+        [Metadata("PUT", "HTTP PUT")]
+        [HttpPut, Route("api/http")]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> Get(string url, string body, string contenttype)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.PutAsync(url, new StringContent(body, System.Text.Encoding.UTF8, contenttype));
+            }
+        }
+
+      
+    
+    }
+}
---
+++ HTTP_ASE_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTTP_ASE_API.WebApiApplication" Language="C#" %>
